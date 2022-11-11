8b7091d81a6e6fc75d5eaf2035e3054d62e5ee69
JMendioroz <jmendiorozredin@gmail.com>
2022-11-11 14:47:38 +0100

    initial commit

---
+++ src/webparts/meetings/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMeetingsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MeetingsWebPartStrings' {
+  const strings: IMeetingsWebPartStrings;
+  export = strings;
+}
