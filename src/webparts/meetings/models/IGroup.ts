85c81cca336b745699f1524559d4f3343548b29d
JMendioroz <jmendiorozredin@gmail.com>
2022-11-15 16:26:40 +0100

    ADD Group interface

---
+++ src/webparts/meetings/models/IGroup.ts
@@ -0,0 +1,13 @@
+export interface IGroup {
+  code:string
+  sector: unknown
+  description?: string
+  createdAt: Date,
+  finishedAt?: Date
+  state: boolean
+  type: unknown
+  theme: unknown
+  ambit: unknown
+  country: unknown
+  string: unknown
+}
