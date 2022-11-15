2f1e29a8d6ea8626d8794a7e76a232ac63423e3f
JMendioroz <jmendiorozredin@gmail.com>
2022-11-15 16:27:26 +0100

    UPDATE Meetings wip

--- src/webparts/meetings/components/IMeetingsProps.ts
+++ src/webparts/meetings/components/IMeetingsProps.ts
@@ -1,7 +1,3 @@
 export interface IMeetingsProps {
   description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
 }
--- src/webparts/meetings/components/Meetings.tsx
+++ src/webparts/meetings/components/Meetings.tsx
@@ -1,43 +1,32 @@
-import * as React from 'react';
-import styles from './Meetings.module.scss';
-import { IMeetingsProps } from './IMeetingsProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-export default class Meetings extends React.Component<IMeetingsProps, {}> {
-  public render(): React.ReactElement<IMeetingsProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+import { IMeetingsProps } from "./IMeetingsProps";
+import styles from "./Meetings.module.scss";
+import { getSP } from "../../../spConfig";
 
-    return (
-      <section className={`${styles.meetings} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
+// const getSectors = async (): Promise<unknown[]> =>{
+//   const sectors = await getSP().web.lists.getByTitle("Sectors").items.select("*")();
+//   return sectors
+// }
+const getSectors = async (): Promise<unknown[]> => {
+  const sectors = await getSP()
+    .web.lists.getByTitle("Groups")
+    .items.select("*", "TaxCatchAll/Term", "TaxCatchAll/ID")
+    .expand("TaxCatchAll")();
+  return sectors;
+};
+
+export default function Meetings(props: IMeetingsProps): React.ReactElement {
+  const [sectors, setSectors] = React.useState<unknown[]>([]);
+
+  React.useEffect(() => {
+    getSectors().then(console.log).catch(console.error);
+  }, []);
+
+  return (
+    <section>
+      <p>Meetings web part</p>
+    </section>
+  );
 }
