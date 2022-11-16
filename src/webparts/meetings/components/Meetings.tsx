5db306d35c3e00fb26e9195e406c9816856aa80e
JMendioroz <jmendiorozredin@gmail.com>
2022-11-16 18:06:04 +0100

    UPDATE Meetings wip

--- src/webparts/meetings/components/Meetings.tsx
+++ src/webparts/meetings/components/Meetings.tsx
@@ -1,32 +1,21 @@
-import * as React from "react";
-import { escape } from "@microsoft/sp-lodash-subset";
+import * as React from "react"
+// import { escape } from "@microsoft/sp-lodash-subset"
 
-import { IMeetingsProps } from "./IMeetingsProps";
-import styles from "./Meetings.module.scss";
-import { getSP } from "../../../spConfig";
-
-// const getSectors = async (): Promise<unknown[]> =>{
-//   const sectors = await getSP().web.lists.getByTitle("Sectors").items.select("*")();
-//   return sectors
-// }
-const getSectors = async (): Promise<unknown[]> => {
-  const sectors = await getSP()
-    .web.lists.getByTitle("Groups")
-    .items.select("*", "TaxCatchAll/Term", "TaxCatchAll/ID")
-    .expand("TaxCatchAll")();
-  return sectors;
-};
+import { IMeetingsProps } from "./IMeetingsProps"
+// import styles from "./Meetings.module.scss"
+import { groupService } from "../services/groupService"
+import { IGroup } from "../models/IGroup"
 
 export default function Meetings(props: IMeetingsProps): React.ReactElement {
-  const [sectors, setSectors] = React.useState<unknown[]>([]);
+  const [groups, setGroups] = React.useState<IGroup[]>([])
 
   React.useEffect(() => {
-    getSectors().then(console.log).catch(console.error);
-  }, []);
+    groupService.getAll().then(setGroups).catch(console.error)
+  }, [])
 
   return (
     <section>
-      <p>Meetings web part</p>
+      <pre>{JSON.stringify(groups, null, 2)}</pre>
     </section>
-  );
+  )
 }
