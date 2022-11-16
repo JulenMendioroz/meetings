6f14752fb725e3647033bca645876ba44dec08f9
JMendioroz <jmendiorozredin@gmail.com>
2022-11-16 18:03:25 +0100

    REFACTOR spConfig

--- src/spConfig.ts
+++ src/spConfig.ts
@@ -1,16 +1,14 @@
-import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { spfi, SPFI, SPFx } from "@pnp/sp";
-import "@pnp/sp/webs";
-import "@pnp/sp/lists";
-import "@pnp/sp/items";
-import "@pnp/sp/batching";
+import { WebPartContext } from "@microsoft/sp-webpart-base"
+import { spfi, SPFI, SPFx } from "@pnp/sp"
+import "@pnp/sp/webs"
+import "@pnp/sp/lists"
+import "@pnp/sp/items"
+import "@pnp/sp/batching"
 
-let sp: SPFI;
+let sp: SPFI
 
 export const setContext = (context: WebPartContext): void => {
-  sp = spfi().using(SPFx(context));
-};
+  sp = spfi().using(SPFx(context))
+}
 
-export const getSP = (): SPFI => {
-  return sp;
-};
+export const getSP = (): SPFI => sp
