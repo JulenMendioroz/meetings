40b4a96b20ce05e3692715c28e6ab7977a49bf1f
JMendioroz <jmendiorozredin@gmail.com>
2022-11-21 15:58:49 +0100

    ADD SP context to meetings component

--- src/webparts/meetings/components/Meetings.tsx
+++ src/webparts/meetings/components/Meetings.tsx
@@ -6,17 +6,22 @@ import type { IGroup } from "../models/IGroup";
 import type { IMeetingsProps } from "./IMeetingsProps";
 // import styles from "./Meetings.module.scss"
 
+const SPContext = React.createContext(null);
+
 export default function Meetings(props: IMeetingsProps): React.ReactElement {
-  const [groups, setGroups] = React.useState<IGroup[]>([]);
+  const { context } = props;
+  // const [groups, setGroups] = React.useState<IGroup[]>([]);
 
-  React.useEffect(() => {
-    groupService.getAll().then(setGroups).catch(console.error);
-  }, []);
+  // React.useEffect(() => {
+  //   groupService.getAll().then(setGroups).catch(console.error);
+  // }, []);
 
-  return (
-    <section>
-      <pre>{JSON.stringify(groups, null, 2)}</pre>
-      <DetailsList items={groups} />
-    </section>
-  );
+  return <SPContext.Provider value={context}></SPContext.Provider>;
+
+  // return (
+  //   <section>
+  //     <pre>{JSON.stringify(groups, null, 2)}</pre>
+  //     <DetailsList items={groups} />
+  //   </section>
+  // );
 }
